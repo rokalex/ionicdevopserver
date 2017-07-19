0bd45f3b08ea7cad93ed780912b43a85de3abb98
Alexandre Rocca <alexandrerocca@MacBook-Pro-de-Alexandre.local>
2017-07-19 12:57:52 +0200

    upd

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@
  * @Author: alexandrerocca
  * @Date:   2017-07-19T12:55:43+02:00
  * @Last modified by:   alexandrerocca
- * @Last modified time: 2017-07-19T12:56:10+02:00
+ * @Last modified time: 2017-07-19T12:57:42+02:00
  */
 
 
  var gulp = require('gulp');
  var ghPages = require('gulp-gh-pages');
 
- var path = YOUR_DEPLOY_PATH;
+ var path = "./platforms/browser/www";
 
  gulp.task('deploy', function() {
    return gulp.src( path+'/**/*' )
