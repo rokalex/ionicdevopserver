e03c2eb3d598474a5414b0ddcca1d7e18a41d7a1
Alexandre Rocca <alexandrerocca@MacBook-Pro-de-Alexandre.local>
2017-07-19 12:22:58 +0200

    upd version - `date`

---
+++ dist/environments/production.js
@@ -0,0 +1,17 @@
+"use strict";
+/**
+ * @Author: alexandrerocca
+ * @Date:   2017-07-19T09:44:21+02:00
+ * @Last modified by:   alexandrerocca
+ * @Last modified time: 2017-07-19T09:50:58+02:00
+ */
+exports.__esModule = true;
+exports.prodVariables = {
+    environmentName: 'Production Environment',
+    ionicEnvName: 'prod',
+    // Front-end
+    apiEndpoint: 'http://localhost:8080',
+    // Back-end
+    dbHost: 'mongodb://alexandre:xj9qZqJjckKmRGgQ@ds161042.mlab.com:61042',
+    dbName: 'ionic-devops'
+};
