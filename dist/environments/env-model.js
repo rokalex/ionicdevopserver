2839329029e83f8191f3e6e615ecb049426c289c
Alexandre Rocca <alexandrerocca@MacBook-Pro-de-Alexandre.local>
2017-07-19 12:59:20 +0200

    Update 2017-07-19T10:59:17.484Z

--- dist/environments/env-model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/**
- * @Author: Nicolas Fazio <webmaster-fazio>
- * @Date:   15-07-2017
- * @Email:  contact@nicolasfazio.ch
- * @Last modified by:   webmaster-fazio
- * @Last modified time: 15-07-2017
- */
-exports.__esModule = true;
