168ddb233b837d729fae1d7fe010ba4313183d23
Alexandre Rocca <alexandrerocca@MacBook-Pro-de-Alexandre.local>
2017-07-19 12:20:11 +0200

    upd version - `date`

--- dist/server/modules/mongodb/mongo.js
+++ dist/server/modules/mongodb/mongo.js
@@ -3,8 +3,8 @@
 * @Author: Nicolas Fazio <webmaster-fazio>
 * @Date:   21-12-2016
 * @Email:  contact@nicolasfazio.ch
- * @Last modified by:   webmaster-fazio
- * @Last modified time: 10-07-2017
+ * @Last modified by:   alexandrerocca
+ * @Last modified time: 2017-07-19T09:57:30+02:00
 */
 exports.__esModule = true;
 var mongoose = require("mongoose");
@@ -25,10 +25,10 @@ exports.mongoDbConnect = function () {
             useMongoClient: true
         }, function (err) {
             if (err) {
-                reject("Error connecting to MongoDB!");
+                reject("Error connecting to MongoDB! -> " + MONGODB_URI);
             }
             else {
-                resolve("MongoDB Ready!");
+                resolve("MongoDB Ready! -> " + MONGODB_URI);
             }
         });
     });
